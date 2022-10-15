bf8538a2ebb2fdaf906fce06775dc34c89e0d300
Mechakhan <wsta@mechakhan.com>
2022-10-15 13:55:53 +0000

    Add public key length field and command to show contents of a certificate (#1)
    * wip
    
    * implement pubkey length field, selfsign the ca cert, implement show command to view certificate
    
    * fix bit size bug

--- README.md
+++ README.md
@@ -5,28 +5,33 @@ The certificates uses the following custom format (not X.509):
 - Serial number (8 octets)
 - Valid After (4 octets, unix epoch)
 - Valid Before (4 octets, unix epoch)
-- Public key (Variable Length, DER encoded)
+- Public key Size (2 octets, bytes)
+- Public key (Variable Size, DER encoded)
+- Signature (Variable Size, DER encoded)
 
-All fields except for the Public key are little-endian encoded. The certificate signature
-is stored in a separate file. Also, revocation is not supported.
+All fields that do not have a variable size are little-endian encoded, and all fields except
+for the Signature are signed and are refered to as TBS (to-be-signed). The size of the Signature
+is defined as the size of the entire certificate subtracted by the size of the TBS.
+Note that revocation is not supported by this CA.
 
 Usage:
-`python3 ca.py [bit-size]`
-where `[bit-size]` is either 256, 384, or 512.
+`python3 ca.py [bit-size] [command] [args]`
+where `[bit-size]` is either 256, 384, or 521 (it's not a typo for 512!) and `[command]` is one of the following:
+- `create`: Creates a CA certificate if it does not already exist, and issues a new certificate with the next serial number.
+- `show [cert-path]`: Decodes and prints the certificate at `[cert-path]` to stdout.
 
 The `secp[bit-size]r1` curve is used to generate public and private keys
 and signatures are generated using SHA256.
 
-This will generate the following CA files (if not already present)
+The `create` command will generate the following CA files (if not already present)
 - `pca-[bit-size].cert`: The CA certificate with Valid After set to the current time and
                          Valid Before set to 10 years into the future.
 - `pca-[bit-size].key`: The CA private key in plain DER encoding.
-- `pca-[bit-size].pub`: The CA public key (same as in the certificate, generated for debug purposes)
 - `pca-[bit-size].serial`: Contains the serial number to use for the next issued certificate.
                            Initialized to zero.
 
 It will also generate a single certificate issued by the CA:
-- `pc-[bit-size]-[serial-no].cert`: The certificate with Valid After set to the current time and
-                                    Valid Before set to 1 day into the future.
+- `pc-[bit-size]-[serial-no].cert`: The certificate signed by the CA with Valid After set
+                                    to the current time and Valid Before set to 1 day into
+                                    the future.
 - `pc-[bit-size]-[serial-no].key`: The certificate's private key in plain DER encoding.
-- `pc-[bit-size]-[serial-no].sig`: The certificate's signature produced by the CA's private key.
--- ca.py
+++ ca.py
@@ -2,12 +2,15 @@ import os
 import sys
 import time
 import struct
+import binascii
+from datetime import datetime
 
 PRIV_KEY_EXT = "key"
 PUB_KEY_EXT = "pub"
 CERT_EXT = "cert"
 SERIAL_NUMBER_EXT = "serial"
 SIGNATURE_EXT = "sig"
+TBS_EXT = "tbs"
 
 PCA_NAME = "pca"
 PC_NAME = "pc"
@@ -15,8 +18,9 @@ PC_NAME = "pc"
 PCA_VALID_DUR = 60 * 60 * 24 * 365 * 10 # 10 years
 PC_VALID_DUR = 60 * 60 * 24 # 1 day
 
-# Little endian with 8-byte serial number, 4-byte "valid after" timestamp, and 4-byte "valid before" timestamp
-CERT_STRUCT_FORMAT = "<QII"
+# Little endian with 8-byte serial number, 4-byte "valid after" timestamp, 4-byte "valid before" timestamp,
+# and 2-byte public key size.
+CERT_STRUCT_FORMAT = "<QIIH"
 
 DIGEST_ALGO = "sha256"
 
@@ -38,60 +42,166 @@ def sn_update(sn_file):
 	with open(sn_file, "w") as f:
 		f.write(str(sn))
 
-def make_cert(ec_name, cert, key, sn, valid_after, valid_before):
+def make_cert(ec_name, cert, key, pca_key, sn, valid_after, valid_before, cert_sig):
+	"""Create a certificate and sign it using SHA256
+
+	Parameters
+	----------
+		ec_name: str
+			The eliptic curve to use when generating the key pair.
+		cert: str
+			Path to where the certificate should be written.
+		key: str
+			Path to where the private key should be written.
+		pca_key: str
+			Path to CA private key used to sign the certificate. If None, the certificate will be self-signed
+		sn: int
+			The serial number of the certificate to be written.
+		valid_after: int
+			The valid after field of the certificate to be written.
+		valid_before: int
+			The valid before field of the certificate to be written.
+		cert_sig: str
+			Temporary path to where the signature should be written.
+	"""
 	os.system("openssl ecparam -name %s -genkey -noout -outform DER -out \"%s\"" % (ec_name, key))
 	os.system("openssl ec -in \"%s\" -inform DER -pubout -outform DER -out \"%s\"" % (key, cert))
 
 	with open(cert, "rb") as f:
 		pub = f.read()
 
+	cert_tbs = struct.pack(CERT_STRUCT_FORMAT, sn, valid_after, valid_before, len(pub)) + pub
 	with open(cert, "wb+") as f:
-		f.write(struct.pack(CERT_STRUCT_FORMAT, sn, valid_after, valid_before))
-		f.write(pub)
+		f.write(cert_tbs)
+
+	# Create signature
+	if pca_key is None:
+		pca_key = key
+	os.system("openssl dgst -sign \"%s\" -keyform DER -%s -out \"%s\" -binary \"%s\"" % (pca_key, DIGEST_ALGO, cert_sig, cert))
+
+	with open(cert_sig, "rb") as f:
+		sig = f.read()
+
+	with open(cert, "wb+") as f:
+		f.write(cert_tbs)
+		f.write(sig)
+
+	os.remove(cert_sig)
+
 
 def get_cert(cert):
+	"""Get a certificate and decode its fields 
+
+	Parameters
+	----------
+		cert: str
+			Path to the certificate sthat should be decoded.
+	"""
+
 	with open(cert, "rb") as f:
 		raw = f.read()
 	str_size = struct.calcsize(CERT_STRUCT_FORMAT)
 	if len(raw) <= str_size:
-		print("Could not decode certificate %s, must have at least a size of %d bytes" % (cert, str_size))
+		print("Could not decode certificate %s, must have a size greater than %d bytes" % (cert, str_size))
 		sys.exit(1)
 
-	(sn, valid_after, valid_before) = struct.unpack(CERT_STRUCT_FORMAT, raw[:str_size])
-	pub = raw[str_size:]
+	(sn, valid_after, valid_before, pk_size) = struct.unpack(CERT_STRUCT_FORMAT, raw[:str_size])
+	if len(raw) <= str_size + pk_size:
+		print("Could not decode certificate %s, must have a size greater than %d + %d bytes" % (cert, str_size, pk_size))
+		sys.exit(1)
+	pub = raw[str_size:str_size + pk_size]
+	sig = raw[str_size + pk_size:]
+
+	return (sn, valid_after, valid_before, pub, sig)
+
+def verify_cert(cert, pca_cert, cert_tbs, cert_sig, pca_pub_key):
+	"""Verify that a certificate is valid and signed by the given CA
+
+	Parameters
+	----------
+		cert: str
+			Path to the certificate to verify.
+		pca_cert: str
+			Path to the CA certificate used for verification.
+		cert_tbs: str
+			Path to temporarily store the certificate's extracted to-be-signed fields.
+		cert_sig: str
+			Path to temporarily store the certificate's extracted signature.
+		pca_pub_key: str
+			Path to temporarily store the CA certificate's extracted public key.
+	"""
 	now = int(time.time())
+	(sn, valid_after, valid_before, pub, sig) = get_cert(cert)
+
 	if now < valid_after or now >= valid_before:
-		print("Ceritifcate %s is not valid yet or has expired" % cert)
-		sys.exit(1)
+		raise Exception("Ceritifcate %s is not valid yet or has expired" % cert)
 
-	return (sn, valid_after, valid_before, pub)
+	with open(cert_tbs, "wb+") as f:
+		f.write(struct.pack(CERT_STRUCT_FORMAT, sn, valid_after, valid_before, len(pub)))
+		f.write(pub)
 
-def sign_cert(cert, sig, pca_cert, pca_key, pca_pub_key):
-	# Create signature
-	os.system("openssl dgst -sign \"%s\" -keyform DER -%s -out \"%s\" -binary \"%s\"" % (pca_key, DIGEST_ALGO, sig, cert))
+	with open(cert_sig, "wb+") as f:
+		f.write(sig)
 
-	# Verify signature
-	(sn, valid_after, valid_before, pca_pub) = get_cert(pca_cert)
+	(sn, valid_after, valid_before, pca_pub, pca_sig) = get_cert(pca_cert)
+
+	if now < valid_after or now >= valid_before:
+		raise Exception("CA ceritifcate %s is not valid yet or has expired" % pca_cert)
 
 	with open(pca_pub_key, "wb+") as f:
 		f.write(pca_pub)
 
-	os.system("openssl dgst -verify \"%s\" -keyform DER -%s -signature \"%s\" -binary \"%s\"" % (pca_pub_key, DIGEST_ALGO, sig, cert))
+	os.system("openssl dgst -verify \"%s\" -keyform DER -%s -signature \"%s\" -binary \"%s\"" % (pca_pub_key, DIGEST_ALGO, cert_sig, cert_tbs))
 
+	os.remove(cert_tbs)
+	os.remove(cert_sig)
+	os.remove(pca_pub_key)
 
-if len(sys.argv) >= 2:
-	if sys.argv[1] != "256" and sys.argv[1] != "384" and sys.argv[1] != "512":
-		print("Unknown key size '%s', must be one of 256, 384, or 512." % sys.argv[1])
-		sys.exit(1)
+if len(sys.argv) < 3:
+	print("Usage: python3 %s [bit-size] [command] [args]" % sys.argv[0]);
+	print("\twhere [bit-size] is either 256, 384, or 521.");
+	print("Commands:")
+	print("\tcreate")
+	print("\t\tCreates a CA certificate if it does not already")
+	print("\t\texist, and issues a new certificate with the next")
+	print("\t\tserial number.")
+	print("\tshow [cert-path]")
+	print("\t\tDecodes and prints the certificate at [cert-path]")
+	print("\t\tto stdout.")
+	sys.exit(1)
 
-	key_size = int(sys.argv[1])
+if sys.argv[1] != "256" and sys.argv[1] != "384" and sys.argv[1] != "521":
+	print("Unknown key size '%s', must be one of 256, 384, or 521." % sys.argv[1])
+	sys.exit(1)
+
+key_size = int(sys.argv[1])
 
 ec = "secp%dr1" % key_size
+
+if sys.argv[2] == "show":
+	if len(sys.argv) < 4:
+		print("\"show\" command given without a [cert-file].")
+		sys.exit(1)
+
+	(sn, valid_after, valid_before, pub, sig) = get_cert(sys.argv[3])
+	print("Serial Number:\t%lu" % sn)
+	print("Valid After:\t%s" % datetime.fromtimestamp(valid_after))
+	print("Valid Before:\t%s" % datetime.fromtimestamp(valid_before))
+	print("Public Key:")
+	print(str(binascii.hexlify(pub), "utf-8"))
+	print("Signature:")
+	print(str(binascii.hexlify(sig), "utf-8"))
+
+	sys.exit(0)
+elif sys.argv[2] != "create":
+	print("Unknown command \"%s\"." % sys.argv[2])
+	sys.exit(1)
 
 pca_cert = "%s-%d.%s" % (PCA_NAME, key_size, CERT_EXT)
 pca_pub_key = "%s-%d.%s" % (PCA_NAME, key_size, PUB_KEY_EXT)
 pca_priv_key = "%s-%d.%s" % (PCA_NAME, key_size, PRIV_KEY_EXT)
 pca_sn = "%s-%d.%s" % (PCA_NAME, key_size, SERIAL_NUMBER_EXT)
+pca_sig = "%s-%d.%s" % (PCA_NAME, key_size, SIGNATURE_EXT)
 
 epoch_now = int(time.time())
 sn = 0
@@ -101,7 +211,7 @@ if not os.path.exists(pca_cert) and not os.path.exists(pca_priv_key) and not os.
 	sn = sn_get(pca_sn)
 	valid_after = epoch_now
 	valid_before = valid_after + PCA_VALID_DUR
-	make_cert(ec, pca_cert, pca_priv_key, sn, valid_after, valid_before)
+	make_cert(ec, pca_cert, pca_priv_key, None, sn, valid_after, valid_before, pca_sig)
 # Open PCA if it exists
 elif os.path.exists(pca_cert) and os.path.exists(pca_priv_key) and os.path.exists(pca_sn):
 	(sn, valid_after, valid_before, pca_pub) = get_cert(pca_cert)
@@ -116,8 +226,9 @@ valid_before = valid_after + PC_VALID_DUR
 pseudo_cert = "%s-%d-%08x.%s" % (PC_NAME, key_size, sn, CERT_EXT)
 pseudo_signature = "%s-%d-%08x.%s" % (PC_NAME, key_size, sn, SIGNATURE_EXT)
 pseudo_priv_key = "%s-%d-%08x.%s" % (PC_NAME, key_size, sn, PRIV_KEY_EXT)
+pseudo_tbs = "%s-%d-%08x.%s" % (PC_NAME, key_size, sn, TBS_EXT)
 
-make_cert(ec, pseudo_cert, pseudo_priv_key, sn, valid_after, valid_before)
-sign_cert(pseudo_cert, pseudo_signature, pca_cert, pca_priv_key, pca_pub_key)
+make_cert(ec, pseudo_cert, pseudo_priv_key, pca_priv_key, sn, valid_after, valid_before, pseudo_signature)
+verify_cert(pseudo_cert, pca_cert, pseudo_tbs, pseudo_signature, pca_pub_key)
 sn_update(pca_sn)
 
